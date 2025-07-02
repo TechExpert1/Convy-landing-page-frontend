219376d44d1162ebaf7aff6d2106902e1d523d1f
minahilali117 <manahilali117@gmail.com>
2025-07-02 17:05:09 +0500

    final

--- convy-landing-page/src/components/Header.jsx
+++ convy-landing-page/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const Header = () => {
   return (
     <nav className="shadow-sm bg-[#00B0C8] h-[70px] w-full flex items-center justify-between px-[200px]">
-      <img src="/images/Convy_app_logo 1.png" className="h-[33px] " />
+      <img src="/images/Convy_app_logo 1.png" className="h-[33px]" />
       <button className="w-[168px] h-[45px] border border-white rounded-full text-center text-white text-[18px] font-['Montserrat'] font-semibold">
         Get The App
       </button>
